6e04cb27b8adbaaeb3510a59336ddc78a9d89e25
davidgilson <cs@david-gilson.com>
2024-01-26 13:56:15 +0000

    GIT COMMIT test SECTOR_01git config --global user.email "you@example.com" TEST

--- chat.py
+++ chat.py
@@ -7,7 +7,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def main():
-    prompt = "Hello, what is your name, and what is your purpose?"
+    prompt = "Hello, what is your name?"
     response = client.chat.completions.create(model="gpt-3.5-turbo-1106",
     messages=[
         {"role": "system", "content": "You are a ChatBot named Quasar, meant to give the user information about astronomy and our Universe."},
