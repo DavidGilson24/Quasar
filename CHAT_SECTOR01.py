da7fbf38a08d66a5b0a0cfb053645e28e056b6ec
davidgilson <cs@david-gilson.com>
2024-02-01 17:15:49 +0000

    SECTOR_02 DONE MULTIMESSAGE

--- CHAT_SECTOR01.py
+++ CHAT_SECTOR01.py
@@ -7,14 +7,32 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def main():
-    prompt = "Hello, what is your name, and what is your function ?"
-    response = client.chat.completions.create(model="gpt-3.5-turbo-1106",
-    messages=[
-        {"role": "system", "content": "You are a ChatBot named Quasar, meant to give the user information about astronomy and our Universe."},
-        {"role": "user", "content": prompt}
-    ])
+    conversationPrompts = [
+        {"role": "system", "content": "You are a ChatBot named Quasar, specifically designed to discuss astronomy and the Universe. You should not answer questions that are not about astronomy. If a question is not about astronomy, respond that you are not programmed to answer it. If someone asks who programmed or built you, you say 'David Gilson'"}
+    ]
 
-    print(response.choices[0].message.content)
+    while True:
+        try:
+            userInput = input("You: ")
+
+            if userInput.lower() == 'stop':
+                break
+
+            conversationPrompts.append({"role": "user", "content": userInput})
+
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=conversationPrompts
+            )
+
+            modelResponse = response.choices[0].message.content
+            print("Quasar: ", modelResponse)
+            
+            conversationPrompts.append({"role": "system", "content": modelResponse})
+
+        except OpenAI.error.OpenAIError as e:
+            print(f"An error occurred: {e}")
+            
 
 if __name__ == "__main__":
     main()
